dd9694ce5d93001b62b46a0c350411ad0072bbb6
Jai <jaijai.stell@gmail.com>
2023-03-13 19:29:35 -0700

    update lab complete

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 //JAIS CODE
-//function addingEventListener() {
-//    const input = document.getElementById('button');
+function addingEventListener() {
+    const input = document.getElementById('button');
 
-//function clickAlert(){
-//  alert('I was clicked!');
-//}
-
-//input.addEventListener('click', clickAlert());
-//}
-//addingEventListener;
-
-const input = document.getElementById('button');
-input.addEventListener('click', function() {
+function clickAlert(){
   alert('I was clicked!');
-});
+}
+
+input.addEventListener('click', clickAlert());
+}
+addingEventListener;
+
+//const input = document.getElementById('button');
+//input.addEventListener('click', function() {
+//  alert('I was clicked!');
+//});
